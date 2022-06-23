022524ef89d5272115e21ed1e281f3cf01a7fae5
Samson <samsonbrody@gmail.com>
2022-06-23 09:15:13 -0700

    any instead off appprops

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,15 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-let prisma;
-export default prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+const prisma = new PrismaClient();
+export default prisma;
